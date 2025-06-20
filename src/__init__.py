b91b701b999d3efeff1722f4bfd74d86c08bef73
Sarah DeLane Back <sarah.back@bolante.net>
2025-06-19 20:14:41 -0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""YouTube Transcript Extractor Package"""
+from .transcript_extractor import YouTubeTranscriptExtractor
+from .config import *
+
+__version__ = "1.0.0"
+__author__ = "Sarah DeLane Back"
